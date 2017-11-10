4aff185e9cd7fa4a93c482a4933f98562e2de33e
thomasport <thomasportugal5@gmail.com>
2017-11-10 15:16:03 -0200

    Prog funcionando sem thread

--- src/main.c
+++ src/main.c
@@ -6,12 +6,70 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+// #include <pthread.h>
+int primo(int x)
+{
+  int flag=0, i=2;
+  if(x==0||x==1)
+    flag=1;
+  while(flag==0&&i<=x/2)
+  {
+
+    if(x%i==0)
+    {
+      flag=1;
+    }
+    else
+    {
+      i++;
+      // printf("12\n");
+    }
+  }
+  return flag;
+    // if (flag==1)
+    // {
+    //   printf("%d não é primo\n",x);
+    // }
+    // else
+    // {
+    //   printf("%d não é primo\n",x);
+    // }
+}
+
 
 int main() {
 
-  int x, y;
+  int x=1, y;
+  int i=2, j=0, flag=0;
+  int numbers[10];
+  FILE *sort;
+  // FILE *aux;
+  pthread_t thread[8];
+  int status;
+  sort=popen("sort -n -r","w");
+  if(sort==NULL)
+    printf("erro\n");
+  while(x!=-1)
+  {
+    scanf("%d", &x);
+    if(x!=-1)
+    {
+      flag=primo(x);
+      if (flag==1)
+      {
+        // fprintf(aux,"%d\n",x);
+        fprintf(sort,"%d\n",x);
+      }
+    }
+    //fscanf(aux,"%d", numbers);
+    // printf("flag");
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+    if(j<7)
+      j++;
+    else
+      j=0;
+  }
+  pclose(sort);
   return 0;
 }
